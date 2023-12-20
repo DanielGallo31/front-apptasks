e5de0bc75dacb8ad6a30e8b6bef63f22e77e88c6
DanielGallo31 <60723904+DanielGallo31@users.noreply.github.com>
2023-12-20 11:32:46 -0500

    Add files via upload

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    {{ tasks }}
+  </div>
+</template>
+
+<script>
+import axios from "@/static/axios.js";
+export default {
+  name: "IndexPage",
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  methods: {
+    async GET_DATA(path) {
+      response = await axios.get(path);
+      return response.data;
+    },
+  },
+  mounted() {
+    this.$nextTick(async () => {
+      let self = this;
+      await Promise.all([this.GET_DATA("task")]).then((val) => {
+        self.tasks = val[0];
+      });
+    });
+  },
+};
+</script>
